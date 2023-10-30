c2335a658bb98db65db8f3e255c07834606e9000
Ling Fei Zhang <lzhang133@gmail.com>
2023-10-30 11:50:00 -0400

    update: added a lot of model attributes

--- code/regression.py
+++ code/regression.py
@@ -3,44 +3,50 @@ import matplotlib.pyplot as plt
 
 
 class LinearRegression():
-    def __init__(self, simple=True) -> None:
-        self.weights = None
-        self.bias = None
+    def __init__(self, simple=True, n=100, p=1, sigma=2) -> None:
         self.simple = simple
 
-    def get_bhat(self, X, y):
-        bhat = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.weights = None
+        self.bias = None
+        self.true_weights = None
+        self.true_bias = None
+        self.true_y = None
+        self.X = None
+        self.n = n
+        self.p = p
+        self.sigma = sigma
+
+    def get_bhat(self):
+        bhat = np.linalg.inv(self.X.T @ self.X) @ self.X.T @ self.y
         return bhat
 
-    def simple_bhat1(self, X, y):
-        xmean = np.mean(X)
-        ymean = np.mean(y)
+    def simple_bhat1(self):
+        xmean = np.mean(self.X)
+        ymean = np.mean(self.true_y)
 
-        bhat1 = np.sum((X - xmean)*(y - ymean))/np.sum(np.square(X - xmean))
-        return bhat1
+        bhat1 = np.sum((self.X - xmean)*(self.true_y - ymean)) / \
+            np.sum(np.square(self.X - xmean))
+        self.weights = bhat1
+        # return bhat1
 
-    def simple_bhat0(self, X, y):
-        xmean = np.mean(X)
-        ymean = np.mean(y)
-        bhat1 = self.simple_bhat1(X, y)
+    def simple_bhat0(self):
+        xmean = np.mean(self.X)
+        ymean = np.mean(self.true_y)
 
-        return ymean - xmean * bhat1
+        self.bias = ymean - xmean * self.weights
+        # return ymean - xmean * bhat1
 
-    def generate_simple_data(self, n, sigma):
-        # y = X * beta + eps
-        # X = np.random.rand(n, p)
-        X = np.arange(n)
-        beta = np.random.rand()
-        eps = np.random.normal(0, np.sqrt(sigma), n)
-        y = np.dot(X, beta) + eps
+    def generate_simple_data(self):
+        self.X = np.arange(self.n)
+        self.true_weights = np.random.rand()  # p = 1
+        self.true_bias = np.random.normal(0, np.sqrt(self.sigma), self.n)
+        self.true_y = np.dot(self.X, self.true_weights) + self.true_bias
 
-        return X, y, beta, eps
-
-    def plot_simple_data(self, X, y, beta, eps, show=False):
+    def plot_simple_data(self, show=False):
         plt.figure(figsize=(8, 6))
-        plt.scatter(X, y, label=f"Data Points", color='b')
-        line = np.dot(X, beta)
-        plt.plot(X, line, label=f"True Line", color='r')
+        plt.scatter(self.X, self.true_y, label=f"Data Points", color='b')
+        line = np.dot(self.X, self.true_weights)
+        plt.plot(self.X, line, label=f"True Line", color='r')
         plt.xlabel("X")
         plt.ylabel("y")
         plt.title("Data Points")
@@ -50,20 +56,20 @@ class LinearRegression():
         if show:
             plt.show()
 
-    def plot_simple_regression_line(self, X, y, beta, eps, show=False):
+    def plot_simple_regression_line(self, show=False):
         plt.figure(figsize=(8, 6))
-        plt.scatter(X, y, label=f"Data Points", c='b')
+        plt.scatter(self.X, self.true_y, label=f"Data Points", c='b')
         plt.xlabel("X")
         plt.ylabel("y")
         plt.title("Simple Linear Regression")
 
-        true_y = np.dot(X, beta)
-        plt.plot(X, true_y, label=f"True Line", color='r')
+        line = np.dot(self.X, self.true_weights)
+        plt.plot(self.X, line, label=f"True Line", color='r')
 
-        bhat1 = self.simple_bhat1(X, y)
-        bhat0 = self.simple_bhat0(X, y)
-        pred = np.dot(X, bhat1) + bhat0
-        plt.plot(X, pred, label=f"Regression line", color='g')
+        self.simple_bhat1()
+        self.simple_bhat0()
+        pred = np.dot(self.X, self.weights) + self.bias
+        plt.plot(self.X, pred, label=f"Regression line", color='g')
 
         plt.legend()
         plt.savefig("figure/simple_regression.png")
@@ -71,7 +77,7 @@ class LinearRegression():
             plt.show()
 
 
-model = LinearRegression()
-X, y, beta, eps = model.generate_simple_data(100, 2)
-model.plot_simple_data(X, y, beta, eps)
-model.plot_simple_regression_line(X, y, beta, eps)
+model = LinearRegression(n=100, sigma=2)
+model.generate_simple_data()
+model.plot_simple_data()
+model.plot_simple_regression_line()
